6c978938cb884776629ca70bbf7862e8874b2348
Konstantin Kharitonov <kkharitonov@it-one.ru>
2026-02-10 19:17:52 +0300

    userContext model

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,16 +5,23 @@ const tg = window.Telegram.WebApp
 tg.ready()
 tg.expand()
 
-// Пользователь
-const user = tg.initDataUnsafe?.user
 console.log(tg.initDataUnsafe)
 
+// Пользователь
+const user = tg.initDataUnsafe?.user ?? null
+let chat_id = tg.initDataUnsafe?.chat_instance
+if (!chat_id || chat_id.startsWith('-')) {
+  chat_id = user?.id || null
+}
+
+// TODO Переделать приветствие в зависимости от настроек канала
 document.getElementById('user').innerText = user ? `Привет, ${user.first_name}` : 'Привет 👋'
 // Календарь
 const userContext = {
   userId: user?.id ?? null,
-  groupId: tg.initDataUnsafe?.chat?.id ?? null,
-  avatarUrl: user?.photo_url ?? null
+  groupId: chat_id,
+  avatarUrl: user?.photo_url ?? null,
+  user: user
 }
 
 console.log(userContext)
